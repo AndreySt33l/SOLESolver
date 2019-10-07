ff9aabac653c26639bd56e40f6afd66603e82863
AndreySt33l <45450947+AndreySt33l@users.noreply.github.com>
2019-10-07 23:44:05 +0600

    Update Main.java

--- src/com/xsteel/Main.java
+++ src/com/xsteel/Main.java
@@ -4,10 +4,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        /*Enter your own paths*/
         String pathIn = "/Users/steel/Desktop/in.txt";
         String pathOut = "/Users/steel/Desktop/out.txt";
+        /*Matrix Reader reads a matrix from in.txt file*/
         Matrix matrix = new Matrix(MatrixReader.readMatrix(pathIn));
-
+        /**
+          * Matrix Writer creates out.txt file
+          * LinearEquation solves the matrix using Jordan-Gauss method
+          * NB The matrix must have the solution; otherwise, the answer will be incorrect
+          */
         MatrixWriter.writeMatrix(LinearEquation.solve(matrix), pathOut);
     }
 }
